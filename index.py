1e1ac0b190fc59115a49988091191bcbb583520a
tushardashpute <74225291+tushardashpute@users.noreply.github.com>
2021-04-16 12:52:56 +0530

    Update index.py

--- index.py
+++ index.py
@@ -39,7 +39,7 @@ class HandlerClass(SimpleHTTPRequestHandler):
             del request[addr_pair]
             request[addr_pair]=[num,ts]
         file=open("index.html", "w")
-        file.write("<!DOCTYPE html> <html> <body><center><h1><font color=\"blue\" face=\"Georgia, Arial\" size=8><em>Real Updated</em></font> Visit Results</h1></center>");
+        file.write("<!DOCTYPE html> <html> <body><center><h1><font color=\"blue\" face=\"Georgia, Arial\" size=8><em>Real </em></font> Visit Results</h1></center>");
         for pair in request:
             if pair[0] == host:
                 guest = "LOCAL: "+pair[0]
